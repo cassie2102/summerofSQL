e4e9ae762fde9e1f03ea992b7d87699a1388459b
cassie2102 <150243041+cassie2102@users.noreply.github.com>
2024-03-14 12:40:53 +0000

    Update 2023 w2.sql

--- Summer of SQL- WK1-WK4/2023 w2.sql
+++ Summer of SQL- WK1-WK4/2023 w2.sql
@@ -1,1 +1,24 @@
--- 2023 Week 2 International Bank Account Numbers
+/*--2023: Week 2 - International Bank Account Numbers
+--Input the data
+ In the Transactions table, there is a Sort Code field which contains dashes. We need to remove these so just have a 6 digit string (hint)
+Use the SWIFT Bank Code lookup table to bring in additional information about the SWIFT code and Check Digits of the receiving bank account (hint)
+Add a field for the Country Code (hint)
+Hint: all these transactions take place in the UK so the Country Code should be GB
+Create the IBAN as above (hint)
+Hint: watch out for trying to combine sting fields with numeric fields - check data types
+Remove unnecessary fields (hint)
+Output the data-- 
+*/
+
+SELECT
+-- 'GB' as COUNTRY_CODE,
+-- CHECK_DIGITS,
+-- SWIFT_CODE,
+-- REPLACE (SORT_CODE,'-','') AS SORT_CODE,
+-- ACCOUNT_NUMBER,
+TRANSACTION_ID,
+'GB'|| CHECK_DIGITS||SWIFT_CODE||REPLACE(sort_code,'-','')|| ACCOUNT_NUMBER AS IBAN
+--S.*
+FROM PD2023_WK02_TRANSACTIONS AS T
+inner join pd2023_wk02_swift_codes as S on T.BANK=S.BANK
+
